cedc079bf4acc70b1cf729e02f157b44c5fe0ba8
AlucardSanin <alucardsanin@gmail.com>
2023-03-15 10:48:21 -0400

    Fix Linter Error

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def can_use_services?
+    of_age? || parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age.to_i >= 18
+  end
+end
