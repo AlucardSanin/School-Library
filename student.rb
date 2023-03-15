3c47aa820ccafb8ec866bccc13db3153d5cd4ff2
AlucardSanin <alucardsanin@gmail.com>
2023-03-15 11:01:36 -0400

    Fix Linter Error

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require 'person'
 
 class Student < Person
   def initialize(classroom, age, name = 'Unknow', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+    super(age, name, parent_permission:)
     @classroom = classroom
   end
 
