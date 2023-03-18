ab7534cb5304188c069255a08db60477b45bebcb
AlucardSanin <alucardsanin@gmail.com>
2023-03-18 16:34:09 -0400

    Decorator Class

--- Classes/person.rb
+++ Classes/person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative '../Decorator/nameable.rb'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
@@ -11,6 +13,10 @@ class Person
 
   def can_use_services?
     of_age? || parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
